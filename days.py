eae8dd59d05e4786a9671d556649ac39e7228fae
Dominik <pajakd@gmail.com>
2019-03-14 21:21:22 -0400

    image

--- days.py
+++ days.py
@@ -1,2 +1,14 @@
-print(3+2)
+import datetime
+from tkinter import *
+from PIL import Image, ImageTk,ImageDraw
 
+def display_window(days):
+    pass
+
+def get_list():
+    today_ordinal = datetime.date.today().toordinal() # normalna liczba int
+    arrival_ordinal = datetime.date(2019,4,17).toordinal()
+    days = [datetime.date.fromordinal(i).isoformat() for i in range(today_ordinal,arrival_ordinal + 1)]
+    return days
+
+print(get_list())
