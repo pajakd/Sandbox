2616fede0ae7d35ea6aae8fad798e6fc215a078f
jeszczedudek <kdudek2010@gmail.com>
2019-03-16 22:26:10 +0100

    hi.

--- days.py
+++ days.py
@@ -3,7 +3,20 @@ from tkinter import *
 from PIL import Image, ImageTk,ImageDraw
 
 def display_window(days):
-    pass
+    window = Tk()
+    window.title("Powrót Dominika")
+
+    image_pil = Image.open("calendar.jpeg").resize((100, 100))
+    img = ImageTk.PhotoImage(image_pil)
+    text =days[0]
+
+    w = Label(window,
+                 compound=CENTER,
+                 text=text,
+                 image=img).pack(side="top")
+    window.geometry('450x300')
+    window.mainloop()
+
 
 def get_list():
     today_ordinal = datetime.date.today().toordinal() # normalna liczba int
@@ -12,3 +25,5 @@ def get_list():
     return days
 
 print(get_list())
+
+display_window(get_list())
